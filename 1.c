4c148d1c4c68fa1fc1f30d28bb1bbf5e6d5d6a0d
Max Krus <majki.job@gmail.com>
2025-12-18 22:25:54 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,12 +1,13 @@
-2025/12/18 22:18:31.116112 cmd_run.go:1415: WARNING: will not expose Kerberos tickets' path: Unsupported KRB5CCNAME: KEYRING:persistent:1010037
-virginwh@kr-h3:~/D03T03.ID_1577483-1$ clang-format -n src/char_decode.c 
-2025/12/18 22:18:33.159049 cmd_run.go:1415: WARNING: will not expose Kerberos tickets' path: Unsupported KRB5CCNAME: KEYRING:persistent:1010037
-virginwh@kr-h3:~/D03T03.ID_1577483-1$ git add src/char_decode.c 
-virginwh@kr-h3:~/D03T03.ID_1577483-1$ git push origin develop
-To ssh://git-ssh.21-school.ru:2222/students_repo/virginwh/D03T03.ID_1577483-1.git
- ! [rejected]        develop -> develop (non-fast-forward)
-error: failed to push some refs to 'ssh://git-ssh.21-school.ru:2222/students_repo/virginwh/D03T03.ID_1577483-1.git'
-hint: Updates were rejected because the tip of your current branch is behind
-hint: its remote counterpart. If you want to integrate the remote changes,
-hint: use 'git pull' before pushing again.
-hint: See the 'Note about fast-forwards' in 'git push --help' for details.
+virginwh@kr-h3:~/D03T03.ID_1577483-1$ git status
+On branch develop
+Changes to be committed:
+  (use "git restore --staged <file>..." to unstage)
+        new file:   src/char_decode.c
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+        1948
+        a.out
+        src/.clang-format
+
+virginwh@kr-h3:~/D03T03.ID_1577483-1$ 
