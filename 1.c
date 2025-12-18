7db69d1e09eec0b8263a971597805283cb4ca8f9
Max Krus <majki.job@gmail.com>
2025-12-18 23:08:00 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,24 +1,24 @@
-#include <stdio.h>
+virginwh@kr-h3:~/D03T03.ID_1577483-1$ gcc -Wall -Werror -Wextra src/quest3.c -o quest3
+virginwh@kr-h3:~/D03T03.ID_1577483-1$ ./quest3
+21
+10946
+virginwh@kr-h3:~/D03T03.ID_1577483-1$ git add src/quest3.c
+virginwh@kr-h3:~/D03T03.ID_1577483-1$ git push origin develop
+Everything up-to-date
+virginwh@kr-h3:~/D03T03.ID_1577483-1$ git commit -m "Quest 3. Recursive"
+[develop 590fabb] Quest 3. Recursive
+ Committer: Virgin Whatarmy <virginwh@kr-h3.stw.21-school.ru>
+Your name and email address were configured automatically based
+on your username and hostname. Please check that they are accurate.
+You can suppress this message by setting them explicitly. Run the
+following command and follow the instructions in your editor to edit
+your configuration file:
 
-long long fib(int n) {
-    if (n == 0) {
-        return 0;
-    }
-    if (n == 1) {
-        return 1;
-    }
-    return fib(n - 1) + fib(n - 2);
-}
+    git config --global --edit
 
-int main(void) {
-    int n;
+After doing this, you may fix the identity used for this commit with:
 
-    if (scanf("%d", &n) != 1 || n < 0) {
-        printf("n/a\n");
-        return 1;
-    }
+    git commit --amend --reset-author
 
-    long long result = fib(n);
-    printf("%lld\n", result);
-    return 0;
-}
+ 1 file changed, 24 insertions(+)
+ create mode 100644 src/quest3.c
