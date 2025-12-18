dfd22f82bac2783fe4efeb9f648f4aa9f7d9a9e0
Max Krus <majki.job@gmail.com>
2025-12-18 22:48:26 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,27 +1,11 @@
-virginwh@kr-h3:~/D03T03.ID_1577483-1$ git add src/char_decode.c 
-virginwh@kr-h3:~/D03T03.ID_1577483-1$ git push origin develop
-To ssh://git-ssh.21-school.ru:2222/students_repo/virginwh/D03T03.ID_1577483-1.git
- ! [rejected]        develop -> develop (non-fast-forward)
-error: failed to push some refs to 'ssh://git-ssh.21-school.ru:2222/students_repo/virginwh/D03T03.ID_1577483-1.git'
-hint: Updates were rejected because the tip of your current branch is behind
-hint: its remote counterpart. If you want to integrate the remote changes,
-hint: use 'git pull' before pushing again.
-hint: See the 'Note about fast-forwards' in 'git push --help' for details.
-virginwh@kr-h3:~/D03T03.ID_1577483-1$ git commit -m "Quest 2. Basic control structures. Some problems with symbols"
-[develop 57cdfdd] Quest 2. Basic control structures. Some problems with symbols
- Committer: Virgin Whatarmy <virginwh@kr-h3.stw.21-school.ru>
-Your name and email address were configured automatically based
-on your username and hostname. Please check that they are accurate.
-You can suppress this message by setting them explicitly. Run the
-following command and follow the instructions in your editor to edit
-your configuration file:
+Quest 3. Recursive
+> Программа разработана
 
-    git config --global --edit
+46 49 42 4F 4E 41 43 43 49 32 31
+> Применить разработанную программу для декодирования вывода резервного модуля
 
-After doing this, you may fix the identity used for this commit with:
+Ты расшифровываешь послание. Похоже на ребус. По всей видимости, стоит разработать небольшую программу для его решения. Результат вычислений эта программа должна выдавать в stdout. Не забудь разместить ее в src/quest3.c.
 
-    git commit --amend --reset-author
-
- 1 file changed, 185 insertions(+)
- create mode 100644 src/char_decode.c
-virginwh@kr-h3:~/D03T03.ID_1577483-1$ 
+Получен Quest 3. Создать программу src/quest3.c, которая рассчитывает и выводит на экран n-ое число Фибоначчи при помощи рекурсивного алгоритма. Сам поиск n-ого числа Фибоначчи должен быть выделен в отдельную функцию. Число вводится через стандартный поток ввода. В случае ошибки выводить «n/a».
+Входные данные	Выходные данные
+21	10946
