b0140843637722d3be65c4042efca221f573d0dd
Max Krus <majki.job@gmail.com>
2025-12-20 15:34:10 +0300

    Update finaPinpong.c

--- finaPinpong.c
+++ finaPinpong.c
@@ -5,12 +5,18 @@ hint: Updates were rejected because the tip of your current branch is behind
 hint: its remote counterpart. If you want to integrate the remote changes,
 hint: use 'git pull' before pushing again.
 hint: See the 'Note about fast-forwards' in 'git push --help' for details.
-virginwh@kr-h3:~/D05P01.ID_1577484-Team_TL_shinoher.17f8fbf6_a34c_4a3f-1$ git push -u origin develop
-To ssh://git-ssh.21-school.ru:2222/students_repo/shinoher/D05P01.ID_1577484-Team_TL_shinoher.17f8fbf6_a34c_4a3f-1.git
- ! [rejected]        develop -> develop (non-fast-forward)
-error: failed to push some refs to 'ssh://git-ssh.21-school.ru:2222/students_repo/shinoher/D05P01.ID_1577484-Team_TL_shinoher.17f8fbf6_a34c_4a3f-1.git'
-hint: Updates were rejected because the tip of your current branch is behind
-hint: its remote counterpart. If you want to integrate the remote changes,
-hint: use 'git pull' before pushing again.
-hint: See the 'Note about fast-forwards' in 'git push --help' for details.
-virginwh@kr-h3:~/D05P01.ID_1577484-Team_TL_shinoher.17f8fbf6_a34c_4a3f-1$ 
+virginwh@kr-h3:~/D05P01.ID_1577484-Team_TL_shinoher.17f8fbf6_a34c_4a3f-1$ git pull origin develop
+From ssh://git-ssh.21-school.ru:2222/students_repo/shinoher/D05P01.ID_1577484-Team_TL_shinoher.17f8fbf6_a34c_4a3f-1
+ * branch            develop    -> FETCH_HEAD
+hint: You have divergent branches and need to specify how to reconcile them.
+hint: You can do so by running one of the following commands sometime before
+hint: your next pull:
+hint: 
+hint:   git config pull.rebase false  # merge
+hint:   git config pull.rebase true   # rebase
+hint:   git config pull.ff only       # fast-forward only
+hint: 
+hint: You can replace "git config" with "git config --global" to set a default
+hint: preference for all repositories. You can also pass --rebase, --no-rebase,
+hint: or --ff-only on the command line to override the configured default per
+hint: invocation.
