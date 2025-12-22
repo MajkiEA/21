853f3038bd8d05efc428d79d715005cb9127785a
Max Krus <majki.job@gmail.com>
2025-12-22 20:22:06 +0300

    Update aaa.c

--- aaa.c
+++ aaa.c
@@ -1,28 +1,61 @@
-virginwh@kr-h5:~/D07T04.ID_1577485-1$ clang-format -n src/squaring.c 
-2025/12/22 20:15:47.833707 cmd_run.go:1415: WARNING: will not expose Kerberos tickets' path: Unsupported KRB5CCNAME: KEYRING:persistent:1010037
-virginwh@kr-h5:~/D07T04.ID_1577485-1$ gcc -Wall -Wextra -Werror -std=c11 src/squaring.c -o squaring
-virginwh@kr-h5:~/D07T04.ID_1577485-1$ ./squaring 
-3 
-1 2 3
-1 4 9virginwh@kr-h5:~/D07T04.ID_157748 cppcheck --enable=all --suppress=missingIncludeSystem src/squaring.c src/squaring.c 
-2025/12/22 20:16:25.607709 cmd_run.go:1415: WARNING: will not expose Kerberos tickets' path: Unsupported KRB5CCNAME: KEYRING:persistent:1010037
-Checking src/squaring.c ...
-nofile:0:0: information: Active checkers: 106/592 (use --checkers-report=<filename> to see details) [checkersReport]
+Новое задание по той же схеме, без излишиств и посмотреть файл, как было, написать человеческие комментарии простыми словами что что делает.
+Quest 3. Data metrics
+> Готово
 
-virginwh@kr-h5:~/D07T04.ID_1577485-1$ valgrind --tool=memcheck --leak-check=yes ./squaring
-==646105== Memcheck, a memory error detector
-==646105== Copyright (C) 2002-2022, and GNU GPL'd, by Julian Seward et al.
-==646105== Using Valgrind-3.22.0 and LibVEX; rerun with -h for copyright info
-==646105== Command: ./squaring
-==646105== 
-3
-1 2 3
-1 4 9==646105== 
-==646105== HEAP SUMMARY:
-==646105==     in use at exit: 0 bytes in 0 blocks
-==646105==   total heap usage: 2 allocs, 2 frees, 2,048 bytes allocated
-==646105== 
-==646105== All heap blocks were freed -- no leaks are possible
-==646105== 
-==646105== For lists of detected and suppressed errors, rerun with: -s
-==646105== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)
+AI Data Analyzer v0.01
+Initialising...
+Loading...
+1. Load module #1... Success!     
+2. Load module #2... Success!
+3. Load decision decision-making module 
+3.1. Load maxmin module... Success!
+3.2. Load data i/o & squaring module... Success!
+3.2. Load stat module
+
+ERROR 
+«Сколько ж можно», — проскальзывает в твоей голове.
+
+> Снова посмотреть папку src репозитория
+
+В папке находится модуль stat. Он практически пустой. Судя по всему, он предназначен для расчета статистических метрик по массиву данных.
+
+> Открыть учебник по математической статистике
+
+Твой любезный сумасшедший друг его, к сожалению, не оставил. Придется гадать и разбираться так.
+
+Получен Quest 3. Добавить реализации необходимых функций в программу src/stat.c таким образом, чтобы программа собиралась и корректно работала (принимала через stdin массив целых чисел, выводила бы его, вычисляла и выводила на новой строке набор статистических метрик — экстремумы (max и min), математическое ожидание и дисперсию, считая, что мы имеем дело с дискретным равномерным распределением). В случае некорректного ввода необходимо выводить «n/a». Уменьшать декомпозицию нельзя — функции можно только добавлять при необходимости, но не убирать. Придерживаться предложенной структуры программы. Числа с плавающей запятой выводить с точностью 6 знаков после запятой.
+Входные данные	Выходные данные
+4
+1 2 3 4	1 2 3 4
+4 1 2.500000 1.250000
+
+
+#include <stdio.h>
+#define NMAX 10
+
+int input(int *a, int *n);
+void output(int *a, int n);
+int max(int *a, int n);
+int min(int *a, int n);
+double mean(int *a, int n);
+double variance(int *a, int n);
+
+void output_result(int max_v,
+                   int min_v,
+                   double mean_v,
+                   double variance_v);
+
+int main()
+{
+    int n, data[NMAX];
+    input(data, n);
+    output(data, n);
+    output_result(max(data, n),
+                  min(data, n),
+                  mean(data, n),
+                  variance(data, n));
+
+    return 0;
+}
+
+
