1f28b52bd5f2c8b14f8fbfabf62cb080420eda36
Max Krus <majki.job@gmail.com>
2025-12-22 17:06:03 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,57 +1,42 @@
-WARNING:root:could not open file '/etc/apt/sources.list': [Errno 13] Permission denied: '/etc/apt/sources.list'
+Получен Quest 2. Внести исправления в программу src/squaring.c таким образом, чтобы программа собиралась и корректно работала (принимала через stdin массив целых чисел, возводила их в квадрат и выводила в stdout). В случае некорректного ввода необходимо выводить «n/a». Уменьшать декомпозицию нельзя — функции можно только добавлять при необходимости, но не убирать.
+Входные данные	Выходные данные
+3
+1 2 3	1 4 9
 
-Command 'ccpcheck' not found, did you mean:
-  command 'cppcheck' from snap cppcheck (2.13.99)
-  command 'cccheck' from deb mono-devel (6.8.0.105+dfsg-3.5ubuntu1)
-  command 'cppcheck' from deb cppcheck (2.13.0-2)
-See 'snap info <snapname>' for additional versions.
-virginwh@kr-h5:~/D07T04.ID_1577485-1$ ccpcheck --enable=all --suppress=missingIncludeSystem src/maxmin.c
-WARNING:root:could not open file '/etc/apt/sources.list': [Errno 13] Permission denied: '/etc/apt/sources.list'
+Сделай по-простому, в рамках задания.
+Изначально файл дали такой:
+#include <stdio.h>
+#define NMAX 10
 
-Command 'ccpcheck' not found, did you mean:
-  command 'cppcheck' from snap cppcheck (2.13.99)
-  command 'cppcheck' from deb cppcheck (2.13.0-2)
-  command 'cccheck' from deb mono-devel (6.8.0.105+dfsg-3.5ubuntu1)
-See 'snap info <snapname>' for additional versions.
-virginwh@kr-h5:~/D07T04.ID_1577485-1$ cppcheck --enable=all --suppress=missingIncludeSystem src/maxmin.c
-2025/12/22 15:45:31.549148 cmd_run.go:1415: WARNING: will not expose Kerberos tickets' path: Unsupported KRB5CCNAME: KEYRING:persistent:1010037
-Checking src/maxmin.c ...
-nofile:0:0: information: Active checkers: 106/592 (use --checkers-report=<filename> to see details) [checkersReport]
+int input(int *a, int *n);
+void output(int *a, int n);
+void squaring(int *a, int n);
 
-virginwh@kr-h5:~/D07T04.ID_1577485-1$ valgrid --tool=memcheck --leak-check=yes ./maxmin
-WARNING:root:could not open file '/etc/apt/sources.list': [Errno 13] Permission denied: '/etc/apt/sources.list'
+int main()
+{
+    int n, data[NMAX];
+    input(data, n);
+    squaring(data, n);
+    output(data, n);
 
-Command 'valgrid' not found, did you mean:
-  command 'valgrind' from snap valgrind (3.26.0)
-  command 'valgrind' from deb valgrind (1:3.22.0-0ubuntu2)
-See 'snap info <snapname>' for additional versions.
-virginwh@kr-h5:~/D07T04.ID_1577485-1$ valgrid --tool=memcheck --leak-check=yes /.maxmin
-WARNING:root:could not open file '/etc/apt/sources.list': [Errno 13] Permission denied: '/etc/apt/sources.list'
+    return 0;
+}
 
-Command 'valgrid' not found, did you mean:
-  command 'valgrind' from snap valgrind (3.26.0)
-  command 'valgrind' from deb valgrind (1:3.22.0-0ubuntu2)
-See 'snap info <snapname>' for additional versions.
-virginwh@kr-h5:~/D07T04.ID_1577485-1$ valgrid --tool=memcheck --leak-check=yes src/.maxmin
-WARNING:root:could not open file '/etc/apt/sources.list': [Errno 13] Permission denied: '/etc/apt/sources.list'
+int input(int *a, int *n)
+{
+    scanf("%d", n);
+    for(int *p = a; p - a < *n; p++)
+    {
+        scanf("%d", p);
+    }
+}
 
-Command 'valgrid' not found, did you mean:
-  command 'valgrind' from snap valgrind (3.26.0)
-  command 'valgrind' from deb valgrind (1:3.22.0-0ubuntu2)
-See 'snap info <snapname>' for additional versions.
-virginwh@kr-h5:~/D07T04.ID_1577485-1$ valgrid --tool=memcheck --leak-check=yes ./maxmin
-WARNING:root:could not open file '/etc/apt/sources.list': [Errno 13] Permission denied: '/etc/apt/sources.list'
+void output(int *a, int n)
+{
+    //NOTHING
+}
 
-Command 'valgrid' not found, did you mean:
-  command 'valgrind' from snap valgrind (3.26.0)
-  command 'valgrind' from deb valgrind (1:3.22.0-0ubuntu2)
-See 'snap info <snapname>' for additional versions.
-virginwh@kr-h5:~/D07T04.ID_1577485-1$ valgrind --tool=memcheck --leak check=yes ./maxmin
-valgrind: check=yes: command not found
-virginwh@kr-h5:~/D07T04.ID_1577485-1$ valgrind --tool=memcheck --leak-check=yes ./maxmin
-==622036== Memcheck, a memory error detector
-==622036== Copyright (C) 2002-2022, and GNU GPL'd, by Julian Seward et al.
-==622036== Using Valgrind-3.22.0 and LibVEX; rerun with -h for copyright info
-==622036== Command: ./maxmin
-==622036== 
-  тут он как-будто ждёт ввода, нужно что-то ввводить?
+void squaring(int *a, int n)
+{
+    //NOTHING
+}
